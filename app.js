2cf091450e1779a1b488c0b0637425babe9918a7
syedaumaima <syedaumema83@gmail.com>
2023-04-25 16:23:07 +0500

    FEAT: JS assigment task

---
+++ app.js
@@ -0,0 +1,113 @@
+// TASK NO One
+
+/*var zakatPercentage = 0.025;
+var userInput = Number(prompt("Enter a number"));
+var result = (zakatPercentage) * (userInput);*/
+
+//Task No Two
+
+/* var FamilyMembers = +prompt("Enter your total family members")
+
+ var FitraMethod = +prompt("choose Fitra Method:\1 Wheat-250 Rupees \2 dates-2100 Rupees \3 Jau-450 Rupees \4 Kishmish-2800 Rupees")
+
+ var Fitraprices;
+
+ if(FitraMethod == "1") {Fitraprices = 250;} 
+   
+ else if(FitraMethod == "2") {Fitraprices = 2100;} 
+ 
+ else if(FitraMethod == "3") { Fitraprices = 450;}
+ 
+ else if(FitraMethod == "4"){Fitraprices = 2800;}
+
+ else {alert("PLEASE WRITE YOUR ANSWER IN ROMAN NUMBERS")}
+
+ var fitraAmount = FamilyMembers * Fitraprices
+
+ alert("The fitra amount for"+ FamilyMembers + "Family member using the" + FitraMethod + "method is RS" + fitraAmount );*/
+
+ //Task NO 3
+
+/* var secretnumber = +prompt("Guess any number between 1 to 10");
+
+ if (secretnumber == 8)
+ {
+     alert("Congrats! You did it")
+ }
+ 
+ else if(secretnumber <= 8)
+ {
+     alert("Your vale is low, guess again")
+ }
+ else if(secretnumber >=8)
+ {
+     alert("Your value is high. guess again")
+ }*/
+
+ //Task NO 4
+
+/*var name=prompt("Enter your name")
+
+document.write(name[0].toUpperCase() + name.slice(1).toLowerCase());*/
+
+//Task No 5
+
+/*var contactnumber = [];
+var contactname = [];
+
+for (var i = 0; i < 3; i++) 
+
+{
+var Cnumber = prompt("Enter contact number:");
+var Cname = prompt("Enter contact name:");
+contactnumber.push(Cnumber);
+contactname.push(Cname);
+}
+
+for (var i = 0; i < contactNumber.length; i++)
+
+{
+    console.log([i + 1], contactName[i], contactNumber[i]);
+}*/
+
+//Task No 6
+
+/*var products =["Lipstick","Mascara","Eyeliner","Kajal Pencil"];
+
+var InputUser = +prompt("Which Makeup Product Do You Want To Buy?");
+
+console.log(products[InputUser]+" is removed from the list");
+
+products.splice(InputUser,1);
+
+console.log("remaining products in the List "+products);
+
+console.log("Number of products "+products.length);*/
+
+//Task No 7
+
+/*var nationality = prompt("Enter your nationality");
+var age = prompt("Enter your age");
+var gender = +prompt("Enter your gender");
+var female = prompt("Are you married?");
+
+if (nationality == "Pakistani" || nationality == "Indian") {
+    console.log("You are eligible to vote");
+}
+if (age >= 18 && gender == "male") {
+    console.log("You are eligible to vote");
+}
+else if (age >= "18" && gender == "female" && female == "married") {
+    console.log("You are eligible to vote");
+}
+
+else {
+    console.log("You are not eligible to vote");
+}*/
+
+//TAsk 8
+
+/*var worldCupTeam = [ "Babar", "Shaheen", "Rizwan", "Shadab", "Hasnain", "Naseem", "Haider Ali", "Shan Masood", "Azam Khan","Amir", "Hassan Ali", "Shoib Malik", "Shahid Afridi", "Umar Gul", "Imran Khan"];
+var FinalTeam = WorldcupTeam.slice(5,1);
+console.log(WorldcupTeam);
+console.log(FinalTeam);*/
